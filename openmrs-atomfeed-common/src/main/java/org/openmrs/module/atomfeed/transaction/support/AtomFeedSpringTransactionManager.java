ef39edf31db7be408bbe6f7693d4d9509000cb46
dkayiwa <kayiwadaniel@gmail.com>
2016-03-09 18:25:32 +0300

    Making the module run on platform 2.0

--- openmrs-atomfeed-common/src/main/java/org/openmrs/module/atomfeed/transaction/support/AtomFeedSpringTransactionManager.java
+++ openmrs-atomfeed-common/src/main/java/org/openmrs/module/atomfeed/transaction/support/AtomFeedSpringTransactionManager.java
@@ -1,7 +1,13 @@
 package org.openmrs.module.atomfeed.transaction.support;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.hibernate.SessionFactory;
-import org.hibernate.classic.Session;
 import org.ict4h.atomfeed.jdbc.JdbcConnectionProvider;
 import org.ict4h.atomfeed.transaction.AFTransactionManager;
 import org.ict4h.atomfeed.transaction.AFTransactionWork;
@@ -12,12 +18,7 @@ import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
-
-import java.lang.reflect.Field;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.util.ReflectionUtils;
 
 public class AtomFeedSpringTransactionManager implements AFTransactionManager, JdbcConnectionProvider {
     private PlatformTransactionManager transactionManager;
@@ -54,10 +55,17 @@ public class AtomFeedSpringTransactionManager implements AFTransactionManager, J
     @Override
     public Connection getConnection() throws SQLException {
         //TODO: ensure that only connection associated with current thread current transaction is given
-        return getSession().connection();
+    	try {
+    		org.hibernate.Session session = getSession();
+            Method connectionMethod = session.getClass().getMethod("connection");
+            return (Connection) ReflectionUtils.invokeMethod(connectionMethod, session);
+        }
+        catch (NoSuchMethodException ex) {
+            throw new IllegalStateException("Cannot find connection() method on Hibernate session", ex);
+        }
     }
 
-    private Session getSession() {
+    private org.hibernate.Session getSession() {
         ServiceContext serviceContext = ServiceContext.getInstance();
         Class klass = serviceContext.getClass();
         try {
@@ -65,10 +73,29 @@ public class AtomFeedSpringTransactionManager implements AFTransactionManager, J
             field.setAccessible(true);
             ApplicationContext applicationContext = (ApplicationContext) field.get(serviceContext);
             SessionFactory factory = (SessionFactory) applicationContext.getBean("sessionFactory");
-            return factory.getCurrentSession();
+            return getCurrentSession(factory);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+	 * Gets the current hibernate session while taking care of the hibernate 3 and 4 differences.
+	 * 
+	 * @return the current hibernate session.
+	 */
+	private org.hibernate.Session getCurrentSession(SessionFactory sessionFactory) {
+		try {
+			return sessionFactory.getCurrentSession();
+		}
+		catch (NoSuchMethodError ex) {
+			try {
+				Method method = sessionFactory.getClass().getMethod("getCurrentSession", null);
+				return (org.hibernate.Session)method.invoke(sessionFactory, null);
+			}
+			catch (Exception e) {
+				throw new RuntimeException("Failed to get the current hibernate session", e);
+			}
+		}
+	}
 }
